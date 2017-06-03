0c7711c565ecfee1a3eeb81ceed8dd8a0c180f83
unknown <messin.maxime@gmail.com>
2017-06-03 09:59:12 +0200

    Docstrings and URL change

--- api.py
+++ api.py
@@ -3,15 +3,36 @@ import requests
 import os
 import subprocess
 from urllib import pathname2url
+import string
 
-T411_URL = "http://api.t411.ai"
+"""
+@var T411_URL: T411 valid api URL
+@var DOWNLOAD_DIR: Directory in which .torrents are downloaded
+@ivar token: T411 API Authentication Token
+"""
+
+T411_URL = "http://api.t411.al"
 DOWNLOAD_DIR = 'downloaded'
+
+class RequestsError(Exception):
+    """Error in API response"""
+    pass
+
+class AuthError(Exception):
+    """Error while authenticating"""
+    pass
 
 class API:
     def __init__(self):
         self.token = None
 
     def auth(self, username, password):
+        """
+        @param username: valid t411 username
+        @param password: valid plaintext t411 password
+        @summary: retrieves an authentication token from t411 api
+        @raise AuthError: Username or password invalid
+        """
 
         # Authenticating
         post_fields = {'username': username, 'password': password}
@@ -20,27 +41,51 @@ class API:
         response = r.json()
 
         if 'error' in response:
-            print(response['error'])
-            raise SystemExit
+            raise AuthError(response['error'])
 
         self.token = response['token']
 
     def persistent_auth(self):
-        try:
-            fp = open('token.txt', 'r')
-            # Reading token from file
-            self.token = fp.readline()
-        except IOError:
+        """reads token from file or creates a new one
+        then stores it in token instance variable
+        @raise AuthError: Token verification failure
+        """
+        if os.path.exists('token.txt'):
+            with open('token.txt', 'r') as fp:
+                self.token = fp.readline()
+        else:
             self.auth(raw_input("Nom d'utilisateur: "), raw_input("Mot de passe: "))
-            fp = open('token.txt', 'w')
-            # Writing token to file
-            fp.write(self.token)
+            with open('token.txt', 'w') as fp:
+                # Writing token to file
+                fp.write(self.token)
+
+        q = self.query('/torrents/search/windows') # Testing auth token
+        response = q.json()
+        if 'error' in response:
+            raise AuthError("Token is invalid")
 
     def query(self, path, params=None):
+        """
+        @summary: Webservice query on t411 api
+        @param path: Full query plaintext path for exemple /torrents/download/45224
+        @type path: str
+        @param params: Optional params for query; offset or limit
+        @type params: dict
+        @return: JSON response
+        @rtype: str
+        """
         return requests.get(T411_URL + path, params,
                 headers={'Authorization': self.token})
 
-    def search(self, query, **kwargs):
+    def raw_search(self, query, **kwargs):
+        """
+        @summary: Search a torrent on t411
+        @param query: Valid URL-Formatted torrent query for exemple "Windows%20Seven"
+        @type query: str
+        @return: Found torrents
+        @rtype: Parsed JSON
+        @raise RequestsError: Response can't be parsed to JSON
+        """
         params = {
             'offset':0
         }
@@ -48,18 +93,32 @@ class API:
         q = self.query('/torrents/search/%s' % query, params)
         response = q.json()
 
-        #TODO eviter la répétition avec la gestion de l'erreur dans auth()
         if 'error' in response:
-            print(response['error'])
-            raise SystemExit
+            raise RequestsError(response['error'])
         return response
 
-    def download(self, torrent):
-        """Download a .torrent to disk
-        :param torrent: a json torrent description
+    def raw_download(self, torrent):
         """
+        @summary: Downloads torrent to disk
+        @param torrent: Torrent to download
+        @type torrent: dictionary
+        @return: Path of the downloaded torrent
+        @rtype: str
+        """
+        def format_filename(s):
+            """
+            @summary: Replace ambiguous filename chars with standard chars
+            @param s: Raw filename
+            @type s: str
+            @return: Formatted filename
+            @rtype: str
+            """
+            valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+            filename = ''.join(c for c in s if c in valid_chars)
+            filename = filename.replace(' ', '_')  # I don't like spaces in filenames.
+            return filename
 
-        file_name = pathname2url(torrent['name']) + '.torrent'
+        file_name = format_filename(torrent['name'] + '.torrent')
         torrent_id = torrent['id']
         q = self.query('/torrents/download/%s' % torrent_id)
 
