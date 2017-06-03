0c5453dc9dc63416c0aa85173f2a6a0828c70d28
unknown <messin.maxime@gmail.com>
2017-06-03 10:01:18 +0200

    Added automatic series download mode

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
  # -*- encoding: utf8 -*-
 from api import API
-from common import *
+from common import Common, choice, most_seeded
 import os
 
 def main():
     api = API()
     api.persistent_auth()
 
-    query = ask_query()
-
-    s = api.search(query, limit=1000)
-    torrents = s.get('torrents')
-
-    dl = choice(most_seeded(torrents, num=10))
-
-    tfile = api.download(dl)
-    os.startfile(tfile)
+    c = Common(api)
+    quit = False
+    while not quit:
+        query = raw_input("Recherche (q: arreter, s: mode saison) : ")
+        if query == 's':
+            c.download_season(raw_input("Saisissez la saison : "))
+        elif query ==  'q':
+            print 'Bye'
+            quit = True
+        else:
+            torrents = c.search(query)
+            chosen = choice(most_seeded(torrents, num=10))
+            c.download(chosen)
 
 if __name__ == '__main__':
     main()
--- common.py
+++ common.py
@@ -1,20 +1,83 @@
- # -*- encoding: utf8 -*-
+# -*- encoding: utf8 -*-
 from urllib import pathname2url
+import os
 
-def most_seeded(torrents, num=None):
-        """Returns max seeded torrent
-        :param torrents: a list of dicts
-        :return: a (dict) torrent or a list of dict with length = num
+class Common:
+
+    def __init__(self, api):
+        self.api = api
+
+    def search(self, query):
         """
+        @summary: search for torrents
+        @param query: t411 query for exemple "Windows 10"
+        @type query: str
+        @rtype: List of dictionaries
+         """
+        query = pathname2url(query) + "*"
+        s = self.api.raw_search(query, limit=1000)
+        return s.get('torrents')
 
-        top = sorted(torrents, key=lambda x:int(x['seeders']), reverse=True)
-        return (top[0] if num is None else top[0:num])
+    def download(self, torrent):
+        """
+        @summary: Download a single torrent
+        @param torrent: A single torrent
+        @type torrent: dictionary
+        """
+        print "Downloading %s" % torrent['name']
+        tfile = self.api.raw_download(torrent)
+        os.startfile(tfile)
+
+    def download_season(self, season_search_string, quality='1080p'):
+        """
+        @summary: Downloads full season
+        @param season_search_string: Search string for exemple "Vikings s03"
+        @type season_search_string: str
+        """
+        print '0 - VOSTFR (par defaut)\n1 - FRENCH'
+        lang = 'FRENCH' if raw_input('Saisir 0 ou 1 : ') == 1 else 'VOSTFR'
+
+        ep = 1
+        fin_saison = False
+        while not fin_saison:
+            query = '%se%.2d* %s* %s' % (season_search_string,
+                                          ep, quality, lang)
+            torrents = self.search(query)
+
+            if torrents == []:
+                if quality == '1080p':
+                    quality = '720p'
+                elif quality == '720p':
+                    print 'Fin de la saison'
+                    fin_saison = True
+            else:
+                dl = most_seeded(torrents)
+                self.download(dl)
+                ep = ep + 1
+                quality = '1080p'
+
+
+def most_seeded(torrents, num=1):
+    """Returns max seeded torrent
+    @param torrents: Result of a search function
+    @type torrents: List of dictionaries
+    @return: Top [num] most seeded torrents
+    @rtype: List of dictionaries
+    """
+
+    top = sorted(torrents, key=lambda x: int(x['seeders']), reverse=True)
+    return top[0] if num == 1 else top[0:num]
+
 
 def choice(torrents):
-    for i,torrent in enumerate(torrents):
-        print("%d - %s (%s seeders)" % (i, torrent['name'], torrent['seeders']))
+    """
+    @param torrents: Ordered torrents
+    @type torrents: A list of dictionaries
+    @returns: One chosen torrent
+    @rtype: Dictionary
+    """
+    for i, torrent in enumerate(torrents):
+        print("%d - %s (%s seeders)" %
+              (i, torrent['name'], torrent['seeders']))
     num_choice = int(input('\nTorrent choisi : '))
     return torrents[num_choice]
-
-def ask_query():
-    return pathname2url(raw_input('Entrer une recherche: '))
